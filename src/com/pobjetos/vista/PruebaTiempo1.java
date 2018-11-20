b7f89b092bf2f61cf0b5030ce1d10f59541c7754
AlejandroTX9601 <alejandro_96R@hotmail.com>
2018-11-19 23:59:38 -0500

    Ejercicio 2

---
+++ src/com/pobjetos/vista/PruebaTiempo1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pobjetos.vista;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author RAI
+ */
+public class PruebaTiempo1 {
+    public static void main(String[] args) {
+        Tiempo1 tiempo = new Tiempo1();
+        String salida = "La hora universal inicial es: " + 
+                tiempo.aStringUniversal() + "\nLa hora estándar inicial es: " +
+                tiempo.aStringEstandar();
+        tiempo.establecerHora(13, 27, 6);
+        salida += "\n\nLa hora universal después de establecerHora es: " + 
+                tiempo.aStringUniversal() + 
+                "\nLa hora estándar después de establecerHora es: " + 
+                tiempo.aStringEstandar();
+        tiempo.establecerHora(99, 99, 99);
+        salida += "\n\nDespués de intentar ajustes inválidos: " +
+                "\nHora universal: " + tiempo.aStringUniversal() + 
+                "\nHora estándar: " + tiempo.aStringEstandar();
+        JOptionPane.showMessageDialog(null, salida,
+                "Prueba de la clase Tiempo1", JOptionPane.INFORMATION_MESSAGE);
+        System.exit( 0 );
+    }
+}
