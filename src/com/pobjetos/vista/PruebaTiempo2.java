fcb9330eeef828ce5969fc4facded8eb6ae4c283
AlejandroTX9601 <alejandro_96R@hotmail.com>
2018-11-20 00:05:40 -0500

    Ejercicio 3

---
+++ src/com/pobjetos/vista/PruebaTiempo2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pobjetos.vista;
+
+/**
+ *
+ * @author RAI
+ */
+public class PruebaTiempo2 {
+    public static void main(String[] args) {
+        Tiempo1 hora = new Tiempo1();
+        tiempo.hora = 7;// error: hora es una variable de instancia privada
+        tiempo.minuto = 15;// error: minuto es una variable de instancia privada
+        tiempo.segundo = 30;// error: segundo es una variable de instancia privada
+    }
+}
